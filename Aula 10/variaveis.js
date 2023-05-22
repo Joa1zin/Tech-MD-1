909c5e8aaaab75efdf0b842646eb018a7090ee99
Joa1zin <jvmm676@gmail.com>
2023-05-22 10:44:03 -0300

    introducing javaScript aula 10

---
+++ Aula 10/variaveis.js
@@ -0,0 +1,9 @@
+var x = "Joao"
+var y = "21"
+
+console.log ("O usuário de nome: " + x + " tem " + y + " anos")
+
+x = "Rafael"
+y = "32"
+
+console.log ("O usuário de nome: " + x + " tem " + y + " anos")
