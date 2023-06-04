77c92f38252fbe21b73800b631a40dd7eda80e27
Joa1zin <jvmm676@gmail.com>
2023-06-04 20:40:56 -0300

    desafio compras js

--- Aula 16/compras.js
+++ Aula 16/compras.js
@@ -1,1 +1,28 @@
-var elementos = prompt('Qual a quantidade de alimentos da lista? ')
+var auxiliar = 0
+var lista = []
+var maximo = prompt('Quantos elementos terá a lista?')
+var elemento = null
+var controle = 0
+
+function valida(palavra){
+    auxiliar = palavra.length
+    if (auxiliar % 2 == 0 ){
+        alert("Elemento inválido!")
+        controle--
+    }else{
+        lista.push(palavra)
+    }
+}
+
+
+function completaLista (){
+    while (controle < maximo){
+        elemento = prompt('Insira o alimento:')
+        valida(elemento)
+        controle++
+    }
+    return console.log(lista)
+}
+
+completaLista()
+
---
+++ Aula 16/teste.js
@@ -0,0 +1,10 @@
+var n = prompt("Digite um numero");
+var total = n/2;
+
+if(n & 1){
+    alert("Impar");
+} else {
+    alert("Par");
+}
+
+alert(total);
